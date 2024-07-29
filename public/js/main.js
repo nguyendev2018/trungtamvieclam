eaeaa7699c654d720bb6a97ef9c270ac62478a7c
nguyendev2018 <nguyendev2018@gmail.com>
2024-07-29 13:15:59 +0700

    final search

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,25 @@
 const barsMenu = document.querySelector('.list-menu__bars');
+const body = document.querySelector('body');
 barsMenu.addEventListener('click', () =>{
     barsMenu.classList.toggle('active');
+    body.classList.toggle('no-scroll');
 })
+$(document).ready(function () {
+ 
+    $("select.select2").each(function () {
+      let elm = "";
+      if (typeof $(this).data("wrapper") !== "undefined") {
+        elm = $(this).parent();
+      }
+      $(this).select2({
+        templateResult: function (data, container) {
+          if (data.element) {
+            $(container).addClass($(data.element).attr("class"));
+          }
+          return data.text;
+        },
+        dropdownParent: elm,
+        minimumResultsForSearch: 10,
+      });
+    });
+  });
