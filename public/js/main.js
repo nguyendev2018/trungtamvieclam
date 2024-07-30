56109560062b2ef8983267b4b4e346e79a786695
nguyendev2018 <nguyendev2018@gmail.com>
2024-07-30 15:04:52 +0700

    update header

--- public/js/main.js
+++ public/js/main.js
@@ -1,60 +1,70 @@
-const barsMenu = document.querySelector('.list-menu__bars');
-const body = document.querySelector('body');
-barsMenu.addEventListener('click', () =>{
-    barsMenu.classList.toggle('active');
-    body.classList.toggle('no-scroll');
-})
-document.addEventListener("DOMContentLoaded", function() {
-    const modal = document.getElementById("myModal");
-    const span = document.getElementsByClassName("close")[0];
-    const modelContent = document.querySelector(".modal-content");
-    const body = document.querySelector("body");
-    // Display the modal with transition
-    setTimeout(function() {
-        modal.classList.add("show");
-        modelContent.classList.add('active');
-        body.classList.add('no-scroll');
-    }, 100);
-
-    // Close the modal when the user clicks on the "x"
-    span.onclick = function() {
-        modal.classList.remove("show");
-        modelContent.classList.remove('active');
-        body.classList.remove('no-scroll');
-        setTimeout(function() {
-            modal.style.display = "none";
-          
-            
-        }, 500); // Match the transition duration
-    }
-
-    // Close the modal when the user clicks anywhere outside of the modal
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.classList.remove("show");
-            setTimeout(function() {
-                modal.style.display = "none";
-            }, 500); // Match the transition duration
-        }
-    }
+const barsMenu = document.querySelector(".list-menu__bars");
+const body = document.querySelector("body");
+const overlayBlack = document.querySelector(".overlay-black");
+const iconClose = document.querySelector(".bi.bi-x-lg");
+barsMenu.addEventListener("click", () => {
+  barsMenu.classList.toggle("active");
+  body.classList.toggle("no-scroll");
+  overlayBlack.classList.add('active');
+});
+iconClose.addEventListener("click", () =>{
+  closeMenuMobile()
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.getElementById("myModal");
+  const span = document.getElementsByClassName("close")[0]; 
+
+  setTimeout(function () {
+    modal.classList.add("show");
+    body.classList.add("no-scroll");
+    overlayBlack.classList.add("active");
+  }, 1000);
+
+  span.onclick = function () {
+    closeModal();
+  };
+  window.onclick = function (event) {
+    if (event.target == overlayBlack) {
+      closeModal();
+      closeMenuMobile();
+    }
+  };
+
+  document.onkeydown = function (event) {
+    if (event.key === "Escape" || event.key === "Esc") {
+      closeModal();
+    }
+  };
+
+  function closeModal() {
+    modal.classList.remove("show");
+    body.classList.remove("no-scroll");
+    overlayBlack.classList.remove("active");
+  }
+});
+
+function closeMenuMobile(){
+  body.classList.remove("no-scroll");
+  barsMenu.classList.toggle("active");
+  overlayBlack.classList.remove('active');
+}
+
 $(document).ready(function () {
- 
-    $("select.select2").each(function () {
-      let elm = "";
-      if (typeof $(this).data("wrapper") !== "undefined") {
-        elm = $(this).parent();
-      }
-      $(this).select2({
-        templateResult: function (data, container) {
-          if (data.element) {
-            $(container).addClass($(data.element).attr("class"));
-          }
-          return data.text;
-        },
-        dropdownParent: elm,
-        minimumResultsForSearch: 10,
-      });
+  $("select.select2").each(function () {
+    let elm = "";
+    if (typeof $(this).data("wrapper") !== "undefined") {
+      elm = $(this).parent();
+    }
+    $(this).select2({
+      templateResult: function (data, container) {
+        if (data.element) {
+          $(container).addClass($(data.element).attr("class"));
+        }
+        return data.text;
+      },
+      dropdownParent: elm,
+      minimumResultsForSearch: 10,
     });
   });
+});
